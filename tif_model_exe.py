95d53d7114b0a39eb2b245ea130cc6de4fca69a0
lastovkoi <lastovkoi@gmail.com>
2023-11-12 18:20:15 +0200

    Updated tif_model

--- tif_model_exe.py
+++ tif_model_exe.py
@@ -6,7 +6,7 @@ start_time = time.time()
 model = tif_model.TifModel()
 model.root_path = "/illukas/data/projects/iti_wave_2023"
 
-model.build_dataset(64, 40, 10)
+model.build_dataset(64, 40, 900)
 model.model_build()
 model.model_fit()
 model.predict_value("/illukas/data/projects/iti_wave_2023/tif_images/gof_gcp_2/gof_gcp_2_20211008T045036_20211008T045101_S1A_3916_S1A_IW_GRDH_1SDV_20211008T045036_20211008T045101_040023_04BCCC_3916.tif", 64)
